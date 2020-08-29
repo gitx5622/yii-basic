c5d120fc717c3523ecfc457827a5c34d4d3ef6f6
Anthony Gitau <gitau.ag@gmail.com>
2020-08-29 19:20:42 +0300

    CI/CD

--- config/db.php
+++ config/db.php
@@ -2,11 +2,11 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=db:3307;dbname=docker-test',
     'username' => 'root',
-    'password' => '',
+    'password' => '1165',
     'charset' => 'utf8',
-
+    'attributes' => [PDO::ATTR_CASE => PDO::CASE_LOWER],
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
